aaef0d78c08e541fdb36352560ae2fcd037b5d48
shashank3010 <115041758+shashank3010@users.noreply.github.com>
2025-04-22 16:24:42 +0530

    Update MatrixOperations.java

--- src/MatrixOperations.java
+++ src/MatrixOperations.java
@@ -1,1 +1,73 @@
+import java.util.Scanner;
+
+public class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input Matrix 1
+        System.out.println("Enter rows and columns for matrices:");
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+
+        System.out.println("Enter Matrix 1:");
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix1[i][j] = sc.nextInt();
+
+        System.out.println("Enter Matrix 2:");
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                matrix2[i][j] = sc.nextInt();
+
+        // Addition
+        System.out.println("Addition:");
+        printMatrix(add(matrix1, matrix2));
+
+        // Subtraction
+        System.out.println("Subtraction:");
+        printMatrix(subtract(matrix1, matrix2));
+
+        // Multiplication
+        System.out.println("Multiplication:");
+        printMatrix(multiply(matrix1, matrix2));
+    }
+
+    static int[][] add(int[][] a, int[][] b) {
+        int[][] result = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[0].length; j++)
+                result[i][j] = a[i][j] + b[i][j];
+        return result;
+    }
+
+    static int[][] subtract(int[][] a, int[][] b) {
+        int[][] result = new int[a.length][a[0].length];
+        for (int i = 0; i < a.length; i++)
+            for (int j = 0; j < a[0].length; j++)
+                result[i][j] = a[i][j] - b[i][j];
+        return result;
+    }
+
+    static int[][] multiply(int[][] a, int[][] b) {
+        int rows = a.length, cols = b[0].length, common = a[0].length;
+        int[][] result = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                for (int k = 0; k < common; k++)
+                    result[i][j] += a[i][k] * b[k][j];
+        return result;
+    }
+
+    static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row)
+                System.out.print(val + " ");
+            System.out.println();
+        }
+    }
+}
 
