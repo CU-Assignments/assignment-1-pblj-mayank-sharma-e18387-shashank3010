e0a095179547bef43eab101d9d59d249410e31ee
shashank3010 <115041758+shashank3010@users.noreply.github.com>
2025-04-22 16:24:16 +0530

    Update StringAnalysis.java

--- src/StringAnalysis.java
+++ src/StringAnalysis.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class StringAnalysis {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        int vowels = 0, consonants = 0, digits = 0, specialChars = 0;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isLetter(c)) {
+                if ("aeiouAEIOU".indexOf(c) != -1)
+                    vowels++;
+                else
+                    consonants++;
+            } else if (Character.isDigit(c)) {
+                digits++;
+            } else if (!Character.isWhitespace(c)) {
+                specialChars++;
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + specialChars);
+    }
+}
 
